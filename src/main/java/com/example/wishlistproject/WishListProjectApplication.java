76a9b28ddf3c09593a70141b37c272217c871229
ayan0138 <ayah0002@stud.kea.dk>
2025-03-28 13:59:37 +0100

    Initial commit

---
+++ src/main/java/com/example/wishlistproject/WishListProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishlistproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishListProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wishlistproject/WishListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wishlistproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
