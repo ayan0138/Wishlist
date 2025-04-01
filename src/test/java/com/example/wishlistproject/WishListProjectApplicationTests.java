5c4bcb838155b069b120d347555c005e4d9235d5
Lawand5 <lama0007@stud.kea.dk>
2025-04-01 14:17:11 +0200

    Task 2.2 og 2.3

--- src/test/java/com/example/wishlistproject/WishListProjectApplicationTests.java
+++ src/test/java/com/example/wishlistproject/WishListProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WishListProjectApplicationTests {
+class $WishListProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
